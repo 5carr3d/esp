34ef3a0a28c4337f80742a7f56f24e64a3372567
5carr3d <38937037+5carr3d@users.noreply.github.com>
2024-11-30 23:59:31 +1100

    Update util.lua

--- libs/util.lua
+++ libs/util.lua
@@ -1,147 +1,101 @@
-local Util = {}
+local Utility = {}
 
-Util.Version = "1.0.0"
+Utility.Version = "1.0.0"
 
-function Util.GetCorners(part)
-	local cf = part.CFrame
-	local size = part.Size
-	local corners = {}
-
-	local frontFaceCenter = (cf + cf.LookVector * size.Z / 2)
-	local backFaceCenter = (cf - cf.LookVector * size.Z / 2)
-
-	local topFrontEdgeCenter = frontFaceCenter + frontFaceCenter.UpVector * size.Y / 2
-	local bottomFrontEdgeCenter = frontFaceCenter - frontFaceCenter.UpVector * size.Y / 2
-	local topBackEdgeCenter = backFaceCenter + backFaceCenter.UpVector * size.Y / 2
-	local bottomBackEdgeCenter = backFaceCenter - backFaceCenter.UpVector * size.Y / 2
-
-	corners.topFrontRight = (topFrontEdgeCenter + topFrontEdgeCenter.RightVector * size.X / 2).Position
-	corners.topFrontLeft = (topFrontEdgeCenter - topFrontEdgeCenter.RightVector * size.X / 2).Position
-
-	corners.bottomFrontRight = (bottomFrontEdgeCenter + bottomFrontEdgeCenter.RightVector * size.X / 2).Position
-	corners.bottomFrontLeft = (bottomFrontEdgeCenter - bottomFrontEdgeCenter.RightVector * size.X / 2).Position
-
-	corners.topBackRight = (topBackEdgeCenter + topBackEdgeCenter.RightVector * size.X / 2).Position
-	corners.topBackLeft = (topBackEdgeCenter - topBackEdgeCenter.RightVector * size.X / 2).Position
-
-	corners.bottomBackRight = (bottomBackEdgeCenter + bottomBackEdgeCenter.RightVector * size.X / 2).Position
-	corners.bottomBackLeft = (bottomBackEdgeCenter - bottomBackEdgeCenter.RightVector * size.X / 2).Position
-
-	return corners
+-- Determine Rig Type
+function Utility.IsR6(character)
+    return character:FindFirstChild("Torso") and character:FindFirstChild("Left Arm")
 end
 
-function Util.R6Bones(player, Camera)
-	local positions = {
-		head = {},
-		torso = {},
-		leftArm = {},
-		rightArm = {},
-		leftLeg = {},
-		rightLeg = {},
-	}
-
-	local Head = Util.GetCorners(player:FindFirstChild("Head"))
-	local Torso = Util.GetCorners(player:FindFirstChild("Torso"))
-	local LeftArm = Util.GetCorners(player:FindFirstChild("Left Arm"))
-	local RightArm = Util.GetCorners(player:FindFirstChild("Right Arm"))
-	local LeftLeg = Util.GetCorners(player:FindFirstChild("Left Leg"))
-	local RightLeg = Util.GetCorners(player:FindFirstChild("Right Leg"))
-
-	positions.head.to = Camera:WorldToViewportPoint((Head.topFrontLeft + Head.bottomBackRight) / 2)
-	positions.head.from = Camera:WorldToViewportPoint((Head.bottomFrontLeft + Head.bottomBackRight) / 2)
-
-	positions.torso.to = Camera:WorldToViewportPoint((Torso.topFrontLeft + Torso.topBackRight) / 2)
-	positions.torso.from = Camera:WorldToViewportPoint((Torso.bottomFrontLeft + Torso.bottomBackRight) / 2)
-
-	positions.rightArm.to = Camera:WorldToViewportPoint((RightArm.topFrontLeft + RightArm.topBackRight) / 2)
-	positions.rightArm.from = Camera:WorldToViewportPoint((RightArm.bottomFrontLeft + RightArm.bottomBackRight) / 2)
-
-	positions.leftArm.to = Camera:WorldToViewportPoint((LeftArm.topFrontLeft + LeftArm.topBackRight) / 2)
-	positions.leftArm.from = Camera:WorldToViewportPoint((LeftArm.bottomFrontLeft + LeftArm.bottomBackRight) / 2)
-
-	positions.leftLeg.to = Camera:WorldToViewportPoint((LeftLeg.topFrontLeft + LeftLeg.topBackRight) / 2)
-	positions.leftLeg.from = Camera:WorldToViewportPoint((LeftLeg.bottomFrontLeft + LeftLeg.bottomBackRight) / 2)
-
-	positions.rightLeg.to = Camera:WorldToViewportPoint((RightLeg.topFrontLeft + RightLeg.topBackRight) / 2)
-	positions.rightLeg.from = Camera:WorldToViewportPoint((RightLeg.bottomFrontLeft + RightLeg.bottomBackRight) / 2)
-
-	return positions
+function Utility.IsR15(character)
+    return character:FindFirstChild("UpperTorso") and character:FindFirstChild("RightUpperArm")
 end
 
-function Util:R15Bones(player, Camera)
-	local positions = {
-		head = {},
-		upperTorso = {},
-		rightUpperArm = {},
-		rightLowerArm = {},
-		leftUpperArm = {},
-		leftLowerArm = {},
-		rightUpperLeg = {},
-		rightLowerLeg = {},
-		leftUpperLeg = {},
-		leftLowerLeg = {},
-		torso = {},
-		leftArm = {},
-		rightArm = {},
-		leftLeg = {},
-		rightLeg = {},
-	}
-
-	local Head = Util.GetCorners(player:FindFirstChild("Head"))
-	local UpperTorso = Util.GetCorners(player:FindFirstChild("UpperTorso"))
-	local RightUpperArm = Util.GetCorners(player:FindFirstChild("RightUpperArm"))
-	local RightLowerArm = Util.GetCorners(player:FindFirstChild("RightLowerArm"))
-	local LeftUpperArm = Util.GetCorners(player:FindFirstChild("LeftUpperArm"))
-	local LeftLowerArm = Util.GetCorners(player:FindFirstChild("LeftLowerArm"))
-	local RightUpperLeg = Util.GetCorners(player:FindFirstChild("RightUpperLeg"))
-	local RightLowerLeg = Util.GetCorners(player:FindFirstChild("RightLowerLeg"))
-	local LeftUpperLeg = Util.GetCorners(player:FindFirstChild("LeftUpperLeg"))
-	local LeftLowerLeg = Util.GetCorners(player:FindFirstChild("LeftLowerLeg"))
-
-	positions.head.to = Camera:WorldToViewportPoint((Head.topFrontLeft + Head.bottomBackRight) / 2)
-	positions.head.from = Camera:WorldToViewportPoint((Head.bottomFrontLeft + Head.bottomBackRight) / 2)
-
-	positions.upperTorso.to = Camera:WorldToViewportPoint((UpperTorso.topFrontLeft + UpperTorso.topBackRight) / 2)
-	positions.upperTorso.from =
-		Camera:WorldToViewportPoint((UpperTorso.bottomFrontLeft + UpperTorso.bottomBackRight) / 2)
-
-	positions.rightUpperArm.to =
-		Camera:WorldToViewportPoint((RightUpperArm.topFrontLeft + RightUpperArm.topBackRight) / 2)
-	positions.rightUpperArm.from =
-		Camera:WorldToViewportPoint((RightUpperArm.bottomFrontLeft + RightUpperArm.bottomBackRight) / 2)
-
-	positions.rightLowerArm.to =
-		Camera:WorldToViewportPoint((RightLowerArm.topFrontLeft + RightLowerArm.topBackRight) / 2)
-	positions.rightLowerArm.from =
-		Camera:WorldToViewportPoint((RightLowerArm.bottomFrontLeft + RightLowerArm.bottomBackRight) / 2)
-
-	positions.leftUpperArm.to = Camera:WorldToViewportPoint((LeftUpperArm.topFrontLeft + LeftUpperArm.topBackRight) / 2)
-	positions.leftUpperArm.from =
-		Camera:WorldToViewportPoint((LeftUpperArm.bottomFrontLeft + LeftUpperArm.bottomBackRight) / 2)
-
-	positions.leftLowerArm.to = Camera:WorldToViewportPoint((LeftLowerArm.topFrontLeft + LeftLowerArm.topBackRight) / 2)
-	positions.leftLowerArm.from =
-		Camera:WorldToViewportPoint((LeftLowerArm.bottomFrontLeft + LeftLowerArm.bottomBackRight) / 2)
-
-	positions.rightUpperLeg.to =
-		Camera:WorldToViewportPoint((RightUpperLeg.topFrontLeft + RightUpperLeg.topBackRight) / 2)
-	positions.rightUpperLeg.from =
-		Camera:WorldToViewportPoint((RightUpperLeg.bottomFrontLeft + RightUpperLeg.bottomBackRight) / 2)
-
-	positions.rightLowerLeg.to =
-		Camera:WorldToViewportPoint((RightLowerLeg.topFrontLeft + RightLowerLeg.topBackRight) / 2)
-	positions.rightLowerLeg.from =
-		Camera:WorldToViewportPoint((RightLowerLeg.bottomFrontLeft + RightLowerLeg.bottomBackRight) / 2)
-
-	positions.leftUpperLeg.to = Camera:WorldToViewportPoint((LeftUpperLeg.topFrontLeft + LeftUpperLeg.topBackRight) / 2)
-	positions.leftUpperLeg.from =
-		Camera:WorldToViewportPoint((LeftUpperLeg.bottomFrontLeft + LeftUpperLeg.bottomBackRight) / 2)
-
-	positions.leftLowerLeg.to = Camera:WorldToViewportPoint((LeftLowerLeg.topFrontLeft + LeftLowerLeg.topBackRight) / 2)
-	positions.leftLowerLeg.from =
-		Camera:WorldToViewportPoint((LeftLowerLeg.bottomFrontLeft + LeftLowerLeg.bottomBackRight) / 2)
-
-	return positions
+function Utility.GetRigType(character)
+    if Utility.IsR6(character) then
+        return "R6"
+    elseif Utility.IsR15(character) then
+        return "R15"
+    else
+        return nil
+    end
 end
 
-return Util
+-- Get Corners of a Part
+function Utility.GetCorners(part)
+    if not part then return nil end
+    local cf = part.CFrame
+    local size = part.Size / 2
+
+    local offsets = {
+        Vector3.new(size.X, size.Y, size.Z), -- TopFrontRight
+        Vector3.new(-size.X, size.Y, size.Z), -- TopFrontLeft
+        Vector3.new(size.X, -size.Y, size.Z), -- BottomFrontRight
+        Vector3.new(-size.X, -size.Y, size.Z), -- BottomFrontLeft
+        Vector3.new(size.X, size.Y, -size.Z), -- TopBackRight
+        Vector3.new(-size.X, size.Y, -size.Z), -- TopBackLeft
+        Vector3.new(size.X, -size.Y, -size.Z), -- BottomBackRight
+        Vector3.new(-size.X, -size.Y, -size.Z) -- BottomBackLeft
+    }
+
+    local corners = {}
+    for i, offset in ipairs(offsets) do
+        corners[i] = (cf * offset).Position
+    end
+
+    return {
+        topFrontRight = corners[1],
+        topFrontLeft = corners[2],
+        bottomFrontRight = corners[3],
+        bottomFrontLeft = corners[4],
+        topBackRight = corners[5],
+        topBackLeft = corners[6],
+        bottomBackRight = corners[7],
+        bottomBackLeft = corners[8]
+    }
+end
+
+-- Get Bone Positions Dynamically
+function Utility.GetBonePositions(character, camera, rigType)
+    local R6Bones = {"Head", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg"}
+    local R15Bones = {
+        "Head", "UpperTorso", "RightUpperArm", "RightLowerArm",
+        "LeftUpperArm", "LeftLowerArm", "RightUpperLeg", "RightLowerLeg",
+        "LeftUpperLeg", "LeftLowerLeg"
+    }
+    local bones = {}
+    local boneList = rigType == "R6" and R6Bones or R15Bones
+
+    for _, partName in ipairs(boneList) do
+        local part = character:FindFirstChild(partName)
+        if part then
+            local corners = Utility.GetCorners(part)
+            if corners then
+                bones[partName] = {
+                    to = camera:WorldToViewportPoint((corners.topFrontLeft + corners.topBackRight) / 2),
+                    from = camera:WorldToViewportPoint((corners.bottomFrontLeft + corners.bottomBackRight) / 2)
+                }
+            end
+        end
+    end
+
+    return bones
+end
+
+-- Version Check
+function Utility.CheckVersion(requiredVersion)
+    local function SplitVersion(version)
+        local major, minor, patch = version:match("^(%d+)%.(%d+)%.(%d+)$")
+        return tonumber(major), tonumber(minor), tonumber(patch)
+    end
+
+    local currentMajor, currentMinor, currentPatch = SplitVersion(Utility.Version)
+    local requiredMajor, requiredMinor, requiredPatch = SplitVersion(requiredVersion)
+
+    if requiredMajor > currentMajor then return false end
+    if requiredMinor > currentMinor then return false end
+    if requiredPatch > currentPatch then return false end
+    return true
+end
+
+return Utility
